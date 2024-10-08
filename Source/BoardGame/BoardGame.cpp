3cf8210098155bef2c581d9d91a24fd4c246a9f8
NoeBlanc <noe.blanc31@gmail.com>
2024-10-08 12:16:28 +0200

    Setup Projet de Gilbert correction

--- Source/BoardGame/BoardGame.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "BoardGame.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, BoardGame, "BoardGame" );
